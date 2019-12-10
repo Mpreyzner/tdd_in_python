453bd93351a9b93951b6ae5807a1a16a32ab3e44
Maria <maria.preyzner@gmail.com>
2019-12-10 21:21:38 +0100

    Update test_31_event_managment.py

--- test_31_event_managment.py
+++ test_31_event_managment.py
@@ -1,1 +1,16 @@
 
+# add markodown with requirements
+# events have guests, venues and speakers
+# person can be invited to event
+# person can not be listed twice as event guest
+# person can not be invited to past event
+# if event is above capacity event can not be joined, person will just join the waitlist
+# if someone resigns from event first person from waitlist joins
+
+# event has to have at least one speaker
+# speakers can change, but there always is at least one
+# venue can change
+
+
+# or maybe christmas tree rental
+# YEAH 
