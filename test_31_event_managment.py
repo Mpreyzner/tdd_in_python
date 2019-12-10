daeb4261079f9125691f8a6a3613ccd4589d5125
Maria <maria.preyzner@gmail.com>
2019-12-10 21:36:49 +0100

    Update test_31_event_managment.py

--- test_31_event_managment.py
+++ test_31_event_managment.py
@@ -14,3 +14,16 @@
 
 # or maybe christmas tree rental
 # YEAH 
+
+# 1.0
+# chrismas tree has genus, heigh range and price
+# christmas tree can be reserved for person for 48h
+# you can rent tree only for 2 weeks
+# there is a return fee that won't be returned if tree came in bad condition or after 2 weeks
+
+# 2.0 trees can only be rented in specific time period
+# if returned after period money wont be returned
+# given tree genus x heigh range has specific quantity
+# if above quantity then it cannot be reserved
+
+# http://wypozyczalniachoinek.eu/jak-to-dziala/regulamin-wynajmu/
