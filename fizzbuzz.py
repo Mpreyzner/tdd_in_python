aa211a653cb9b0f03758e9e26a341fa2a8712ec1
Mpreyzner <maria.preyzner@gmail.com>
2019-12-11 18:30:55 +0100

    (Refactor) Implement returning number when non divisible by 3 nor 5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,9 @@
 def fizzbuzz(number):
-    if number == 2:
-        return 2
     if number % 3 == 0 and number % 5 == 0:
         return 'FizzBuzz'
     if number % 3 == 0:
         return 'Fizz'
     if number % 5 == 0:
         return 'Buzz'
+    return number;
 
