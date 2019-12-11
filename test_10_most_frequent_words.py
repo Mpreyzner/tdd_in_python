d518a24872bf9206f07231a611a937724ca49e71
Mpreyzner <maria.preyzner@gmail.com>
2019-12-11 19:04:19 +0100

    (RED) Added failing test for most frequent

--- test_10_most_frequent_words.py
+++ test_10_most_frequent_words.py
@@ -1,1 +1,19 @@
 # https://github.com/TDD-Katas/frequent-words
+
+# Given a list of words, print the list of top N most frequently occurring words in the list
+# together with their frequency of occurrence.
+# The output should be sorted in descending order by frequency of occurrence.
+# If two words occur with the same frequency then they should be ordered in alphabetical order.
+# Input:
+#
+# N = 3
+# list = united, states, america, north, america, south, america, south, africa, north, korea
+#
+# Output:
+#
+# 3 america
+# 2 north
+# 2 south
+
+def test_most_frequent():
+    assert most_frequent(['apple', 'banana', 'apple']) == [('apple', 2), ('banana', 1)]
