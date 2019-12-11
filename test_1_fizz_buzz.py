b44303bcd34a2dbd1a81c816d68bcefbd5892b5f
Mpreyzner <maria.preyzner@gmail.com>
2019-12-11 17:59:10 +0100

    (GREEN) Added naive implementation

--- test_1_fizz_buzz.py
+++ test_1_fizz_buzz.py
@@ -5,9 +5,9 @@
 from fizzbuzz import fizzbuzz
 import pytest
 
-divisible_by_tree = [3, 6, 9, 12, 15, 300]
+divisible_by_tree = [3, 6, 9, 12, 300]
 
-divisible_by_five = [5, 10, 15, 20, 500]
+divisible_by_five = [5, 10, 20, 500]
 
 
 # http://doc.pytest.org/en/latest/example/parametrize.html
@@ -19,6 +19,7 @@ def test_fizz(number):
 @pytest.mark.parametrize("number", divisible_by_five)
 def test_buzz(number):
     assert fizzbuzz(number) == 'Buzz'
+
 
 def test_fizzbuzz():
     assert fizzbuzz(15) == 'FizzBuzz'
