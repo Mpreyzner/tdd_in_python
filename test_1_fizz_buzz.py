3dfbc3388fce65390370c0342740ed67d76c7abd
Mpreyzner <maria.preyzner@gmail.com>
2019-12-11 17:35:12 +0100

    Added first test for fizzbuzz

--- test_1_fizz_buzz.py
+++ test_1_fizz_buzz.py
@@ -1,1 +1,10 @@
+# Write a program that prints one line for each number from 1 to 100
+# For multiples of three print Fizz instead of the number
+# For the multiples of five print Buzz instead of the number
+# For numbers which are multiples of both three and five print FizzBuzz instead of the number
+def fizzbuzz(number):
+    pass
 
+
+def test_fizz():
+    assert fizzbuzz(3) == 'Fizz'
