3656673041a606cb6ffb9b9131fd9bc432143155
Mpreyzner <maria.preyzner@gmail.com>
2019-12-12 23:52:55 +0100

    Added desc to password strength

---
+++ test_22_password_strengh.py
@@ -0,0 +1,21 @@
+# https://github.com/xpepper/PasswordStrengthChecker
+# optional, strong password generator
+
+# Iteration 1
+#
+#     The length of the password must be at least 7 characters in length
+#     The password must contain at least 1 letter
+#     The password must contain at least 1 digit
+#
+# Iteration 2
+#
+# We need to add support for checking admin passwords
+#
+#     Admin passwords must be at least 10 characters in length
+#     Admin passwords must contain at least 1 special character
+#
+# Iteration 3
+#
+# We need to provide feedback to the user about the strength of their password
+#
+#     Provide the user with a list of reasons why their password is 'weak'
