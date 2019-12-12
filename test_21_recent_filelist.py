69af426b016c49d0072b80b23bc099919e559bf5
Mpreyzner <maria.preyzner@gmail.com>
2019-12-12 20:03:13 +0100

    (RED) Added assertion that when list is full, oldest item is thrown away

--- test_21_recent_filelist.py
+++ test_21_recent_filelist.py
@@ -64,9 +64,10 @@ def test_item_should_not_be_duplicated():
 def test_list_should_have_up_to_15_items():
     program = Program()
 
-    for i in range(16):
+    for i in range(1, 16):
         file_name = str(i) + '.txt'
         program.open(file_name)
 
     file_list = program.get_recent_file_list()
     assert len(file_list) == 15
+    assert '1.txt' not in file_list
