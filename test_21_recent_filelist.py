b02e8a3dc51c9d127f23eeab7a97d920f76167fc
Mpreyzner <maria.preyzner@gmail.com>
2019-12-12 22:36:13 +0100

    (Refactor) Simplify code

--- test_21_recent_filelist.py
+++ test_21_recent_filelist.py
@@ -19,17 +19,15 @@ class Program:
         self._recent_file_list = deque()
 
     def open(self, file):
+        if file in self._recent_file_list:
+            self._recent_file_list.remove(file)
         self._recent_file_list.appendleft(file)
         if len(self._recent_file_list) > self._max_list_length:
             self._recent_file_list.pop()
         return self
 
     def get_recent_file_list(self):
-        counter = Counter(self._recent_file_list)
-        result = []
-        for name, quantity in counter.items():
-            result.append(name)
-        return result
+        return self._recent_file_list
 
 
 def test_empty_list():
