7fcbf1bbdfaffead3f54bbbb6ccbeff6f86edf27
Mpreyzner <maria.preyzner@gmail.com>
2019-12-12 19:40:01 +0100

    (RED) Added test that item on the list will not duplicate

--- test_21_recent_filelist.py
+++ test_21_recent_filelist.py
@@ -9,9 +9,10 @@
 # list If an opened file already exists in the recent file list, it is bumped to the top, not duplicated in the list
 # If the recent file list gets full (typical number of items is 15), the oldest item is removed when a new item is
 # added
+from collections import Counter
 
 
-class Program():
+class Program:
     _recent_file_list = []
 
     def __init__(self):
@@ -19,6 +20,7 @@ class Program():
 
     def open(self, file):
         self._recent_file_list.append(file)
+        self._recent_file_list = list(sorted(Counter(self._recent_file_list)))
         return self
 
     def get_recent_file_list(self):
@@ -37,10 +39,20 @@ def test_not_empty_list():
 
 def test_bumping_item():
     program = Program()
-    
+
     program.open('2.txt')
     program.open('1.txt')
     program.open('1.txt')
 
     file_list = program.get_recent_file_list()
     assert file_list[0] == '1.txt'
+
+
+def test_item_should_not_be_duplicated():
+    program = Program()
+
+    program.open('1.txt')
+    program.open('1.txt')
+
+    file_list = program.get_recent_file_list()
+    assert len(file_list) == 2
