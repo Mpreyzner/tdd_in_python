d4deeb3fd380e1642de672bf938f08b2fdcadb41
Mpreyzner <maria.preyzner@gmail.com>
2019-12-12 19:24:10 +0100

    (RED) Added first test for recent file list

--- test_20_most_frequent_word.py
+++
@@ -1,1 +0,0 @@
-# https://github.com/TDD-Katas/frequent-words
--- test_21_recent_filelist.py
+++ test_21_recent_filelist.py
@@ -1,1 +1,15 @@
 # https://sites.google.com/site/tddproblems/all-problems-1/recent-file-list
+# A popular feature of graphical editors of
+# all kinds (text, graphics, spreadsheets, ..) is the Recent file list. It is often found as a sub-menu of the file
+# menu in the GUI of the program.
+#
+# Use TDD to grow this kind of behaviour. Some examples of the behaviour is
+#
+# When the program is run for the first time, the list is empty When a file is opened, it is added to the recent file
+# list If an opened file already exists in the recent file list, it is bumped to the top, not duplicated in the list
+# If the recent file list gets full (typical number of items is 15), the oldest item is removed when a new item is
+# added
+
+def test_empty_list():
+    file_list = Program().open('somefile.txt').get_recent_file_list()
+    assert len(file_list) == 0
